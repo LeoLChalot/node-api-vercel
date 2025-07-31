59de34bd81ae3dd0283ce67dc5afb7ff485a3d51
LeoLChalot <l.larouchalot@gmail.com>
2025-07-31 20:31:46 +0200

    ajout d'un option random

--- api/index.js
+++ api/index.js
@@ -1,25 +1,38 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
-let mouvement = { "move": "STAY", "action": "NONE" }
+let mouvement = { "move": "STAY", "action": "NONE" };
+const moves = ["UP", "DOWN", "RIGHT", "LEFT", "STAY"];
+const actions = ["COLLECT", "ATTACK", "BOMB", "NONE"];
+
+function generateRandomMovement() {
+  const randomMoveIndex = Math.floor(Math.random() * moves.length);
+  const randomActionIndex = Math.floor(Math.random() * actions.length);
+  return {
+    move: moves[randomMoveIndex],
+    action: actions[randomActionIndex]
+  };
+}
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: "Hello Bot Trainer !" });
 });
 
 app.get('/action', (req, res) => {
-  res.status(200).json(mouvement)
+
+  res.status(200).json(generateRandomMovement());
 });
+
 app.post('/action', (req, res) => {
   const { move, action } = req.body;
-  mouvement = { "move": move, "action": action }
-  res.status(200).json({message: "Mouvement enregistré"})
-})
+  mouvement = { "move": move, "action": action };
+  res.status(200).json({ message: "Mouvement enregistré" });
+});
 
 app.listen(3000, () => console.log('Server ready on port 3000.'));
 
