fc10137063d03fbdb591e9d09dfd2d3a4fc4c9b0
LeoLChalot <l.larouchalot@gmail.com>
2025-08-01 10:37:36 +0200

    ajout exception cors

--- api/index.js
+++ api/index.js
@@ -6,7 +6,14 @@ const { Pool } = require('pg');
 const app = express();
 app.use(bodyParser.json());
 
-app.use(cors());
+function setCorsHeaders(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+}
+
+app.use(setCorsHeaders);
 
 
 const pool = new Pool({
